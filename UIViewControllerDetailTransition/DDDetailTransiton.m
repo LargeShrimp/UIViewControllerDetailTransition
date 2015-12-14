b73e4b80301f72e21c8256d65da00397b7dd90cf
taitanxiami <taitanxiami@gmail.com>
2015-12-14 23:26:44 +0800

    modify show detail animation

--- UIViewControllerDetailTransition/DDDetailTransiton.m
+++ UIViewControllerDetailTransition/DDDetailTransiton.m
@@ -12,6 +12,13 @@
 #import "DDDetailController.h"
 @implementation DDDetailTransiton
 
+/**
+ *  动画时间
+ *
+ *  @param transitionContext <#transitionContext description#>
+ *
+ *  @return <#return value description#>
+ */
 - (NSTimeInterval)transitionDuration:(nullable id <UIViewControllerContextTransitioning>)transitionContext {
     
     return 0.6f;
@@ -19,40 +26,38 @@
 
 - (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext {
     
-    //拿到两个视图控制器和视图容器
+//  拿到两个视图控制器和视图容器
     UIView *containtVIew  = [transitionContext containerView];
     DDCollectionViewController *fromVC = (DDCollectionViewController *) [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
     DDDetailController *toVC = (DDDetailController *)[transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
     
+//  获取选中cell
     NSIndexPath *selectIdx = [[fromVC.collectionView indexPathsForSelectedItems] firstObject];
     DDCell *cell = (DDCell*) [fromVC.collectionView cellForItemAtIndexPath:selectIdx];
     fromVC.indexPath = selectIdx;
-    //生成截图
+
+//  生成截图,并设置截图的frame
     UIView *snapimageView = [cell.cellImageView snapshotViewAfterScreenUpdates:NO];
-    
     snapimageView.frame = fromVC.finalCellRect = [containtVIew convertRect:cell.cellImageView.frame fromView:cell.cellImageView.superview];
-    snapimageView.frame  = [containtVIew convertRect:cell.cellImageView.frame fromView:cell.cellImageView.superview];
-
-//    NSLog(@"origin rect =%@",NSStringFromCGRect(snapimageView.frame));
-
     cell.cellImageView.hidden = YES;
-    
+
+//  设置将要现实的view 属性
     toVC.view.frame = [transitionContext finalFrameForViewController:toVC];
     toVC.view.alpha = 0.0f;
     toVC.detailImageView.hidden = YES;
     
-    [containtVIew addSubview:snapimageView];
     [containtVIew addSubview:toVC.view];
+    [containtVIew addSubview:snapimageView];
 
     
     [UIView animateWithDuration:[self transitionDuration:transitionContext] delay:0 usingSpringWithDamping:0.6f initialSpringVelocity:1.0f options:UIViewAnimationOptionCurveLinear animations:^{
-        
+        // 动画
         [containtVIew layoutIfNeeded];
-        
+
         toVC.view.alpha = 1.0f;
         //toVC.detailImageView 的frame 在父视图view中的位置
         snapimageView.frame  =[ containtVIew convertRect:toVC.detailImageView.frame toView:toVC.view];
-        NSLog(@"final rect =%@",NSStringFromCGRect(snapimageView.frame));
+
 
 
     } completion:^(BOOL finished) {
